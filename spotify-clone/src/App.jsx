c38f322d9a1451d46c9beebd97b6e8afd32249a8
Daniel Sanchez <hd2242kin20@gmail.com>
2024-10-02 12:44:26 -0300

    111

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,42 +1,25 @@
+import CardSidebar from "./components/CardSidebar"
+import Container from "./components/Container"
+import ConteudoPrincipal from "./components/ConteudoPrincipal"
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+
 function App() {
+
   return (
-    <div className="bg-pink-700 h-screen w-screen flex flex-col">
-      <div className="bg-pink-500 h-16 w-screen"></div>
-      <div className="bg-gray-300 flex-1 w-screen grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 p-4 overflow-auto">
-        <div className="bg-cyan-400 flex flex-col items-center justify-around p-4">
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24"></div>
-        </div>
-        
-        <div className="bg-gray-300 flex flex-col items-center justify-around p-4">
-          <div className="bg-pink-500 h-24 w-24 mb-4">
-            <div className="bg-cyan-400 h-6 w-12 mx-auto"></div>
-          </div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-        </div>
 
-        <div className="bg-gray-300 flex flex-col items-center justify-around p-4">
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-        </div>
-
-        <div className="bg-gray-300 flex flex-col items-center justify-around p-4">
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-        </div>
-
-        <div className="bg-gray-300 flex flex-col items-center justify-around p-4">
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-          <div className="bg-pink-500 h-24 w-24 mb-4"></div>
-        </div>
-      </div>
-    </div>
-  );
+    <>
+      <Header />
+      <Container>
+        <Sidebar>
+          <CardSidebar />
+          <CardSidebar />
+          <CardSidebar />
+        </Sidebar>
+        <ConteudoPrincipal />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
